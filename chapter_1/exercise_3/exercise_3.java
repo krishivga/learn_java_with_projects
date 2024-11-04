5f27bbc7a6361ba45dda9f083e5a630e06c5b093
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-11-04 14:51:33 +0530

    Exercise 3 finished.

---
+++ chapter_1/exercise_3/exercise_3.java
@@ -0,0 +1,9 @@
+package chapter_1.exercise_3;
+
+public class exercise_3 {
+    public static void main(String[] args){
+        String openingHours = "08:00";
+        String closingHours = "20:00";
+        System.out.println("Our opening hours are " + openingHours + " and we are open until " + closingHours + ".");
+    }
+}
