7390f7a2d97ae5b221ddf11af082418b081f222a
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-11-04 13:48:29 +0530

    Wrote down test setup to udnerstand debugging

--- chapter_1/HelloWorld.java
+++ chapter_1/HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
     public static void main (String[] args) {
-        System.out.println("Hello World!");
+        String greeting = "Hello, World!";
+        System.out.println(greeting);
+
+        int number = 5;
+        int doubled = doubleNumber(number);
+        System.out.println("The doubled number is: " + doubled);
+    }
+
+    public static int doubleNumber(int input){
+        return input * 2;
     }
 }
