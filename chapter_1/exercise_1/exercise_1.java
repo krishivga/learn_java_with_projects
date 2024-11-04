75762deab187002489095e442ce1fb233bcd8ff1
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-11-04 14:56:48 +0530

    Exercise 6 finished.

--- chapter_1/exercise_1/exercise_1.java
+++ chapter_1/exercise_1/exercise_1.java
@@ -1,8 +1,11 @@
 public class exercise_1{
     public static void main(String[] args) {
 
-    // Exericse 4, adding my name
-    String customer_name = "krishiv";
+        // Exericse 4, adding my name
+        String customer_name = "krishiv";
         System.out.println("Welcome " + customer_name + " to Meizoic Eden!");
+        // Exercise 6, message to comfort scared visitors
+        System.out.println("Mezoic Eden is safe and secure.");
+    
     }
 }
