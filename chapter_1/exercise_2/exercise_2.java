da0e02ec2a80c2605dda80a8d61467efdd62b400
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-11-04 14:26:12 +0530

    Finished exercise 2.

---
+++ chapter_1/exercise_2/exercise_2.java
@@ -0,0 +1,9 @@
+package chapter_1.exercise_2;
+
+public class exercise_2 {
+    public static void main(String[] args) {
+        String name = "krishiv";
+        String position = "Park Manager";
+        System.out.println("My name is " + name + " and I want to be a " + position + " in Mezoic Eden,");
+    }
+}
