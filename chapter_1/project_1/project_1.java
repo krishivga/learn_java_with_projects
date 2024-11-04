8801bc0ddbafa00f2a1e8a95e5997ba4fd0e8949
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-11-04 15:08:27 +0530

    Finished project 1.

--- chapter_1/project_1/project_1.java
+++ chapter_1/project_1/project_1.java
@@ -2,13 +2,22 @@ package chapter_1.project_1;
 
 public class project_1 {
     public static void main(String[] args) {
+        // Making quite literally, a signpost.
         System.out.println("- - - - - - - - - - - -");
         System.out.println("|     Welcome to      |");
         System.out.println("|   Mezoic Eden park  |");
+        System.out.println("|                     |");
         System.out.println("|      Timings        |");
+        System.out.println("|     Open: 08:00     |");
+        System.out.println("|    Closing: 20:00   |");
         System.out.println("|                     |");
-        System.out.println("|                     |");
+        System.out.println("|     Don't worry     |");
+        System.out.println("|   Mezoic Eden park  |");
+        System.out.println("|       is safe!      |");
         System.out.println("- - - - - - - - - - - -");
-
+        System.out.println("         |-|-|         ");
+        System.out.println("         |-|-|         ");
+        System.out.println("         |-|-|         ");
+        System.out.println("         |-|-|         ");
     }
 }
