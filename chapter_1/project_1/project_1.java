c951415d0e765e5ef35501ae0c05769cadf0ec50
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-11-04 15:02:02 +0530

    Initialised project and laid out initial design.

---
+++ chapter_1/project_1/project_1.java
@@ -0,0 +1,14 @@
+package chapter_1.project_1;
+
+public class project_1 {
+    public static void main(String[] args) {
+        System.out.println("- - - - - - - - - - - -");
+        System.out.println("|     Welcome to      |");
+        System.out.println("|   Mezoic Eden park  |");
+        System.out.println("|      Timings        |");
+        System.out.println("|                     |");
+        System.out.println("|                     |");
+        System.out.println("- - - - - - - - - - - -");
+
+    }
+}
